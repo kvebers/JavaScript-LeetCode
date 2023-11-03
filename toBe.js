cb66743afa92af1bcf832bdfcd58b02354d68544
Kārlis Vilhelms Vēbers <49612380+kvebers@users.noreply.github.com>
2023-11-03 11:39:51 +0100

    Update toBe.js

--- toBe.js
+++ toBe.js
@@ -14,8 +14,12 @@ var expect = function(val) {
             if (val1 !== val)
                 return true;
             else
-                throw new Error("Not Equal");
+                throw new Error("Equal");
         }
     };
 };
 
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
