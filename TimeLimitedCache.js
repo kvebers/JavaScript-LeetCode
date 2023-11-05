a2c5c4eeed00aa4e5b83cdbefbab93a8669886fb
Kārlis Vilhelms Vēbers <49612380+kvebers@users.noreply.github.com>
2023-11-05 12:17:11 +0100

    Create TimeLimitedCache.js

---
+++ TimeLimitedCache.js
@@ -0,0 +1,44 @@
+
+var TimeLimitedCache = function() {
+    this.map = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let search = this.map.has(key);
+    if (search) clearTimeout(this.map.get(key).ref);
+    this.map.set(key, 
+        {value,
+        ref: setTimeout(() => this.map.delete(key), duration)});
+    return search;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    if (this.map.has(key))
+        return this.map.get(key).value;
+    else
+        return -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.map.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
